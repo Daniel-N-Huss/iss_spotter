57ec2270b0c8f069b131a4c00c37e45001dff157
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-08 04:47:22 +0000

    polishing on return data including a human readbale flyover time

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ nextISSTimesForMyLocation((error, passTimes) => {
     return console.log("It didn't work! ", error);
   }
 
-  console.log(passTimes);
+  const data = passTimes;
+
+  data.forEach(element => {
+    let pass = element.risetime;
+    let time = new Date(pass * 1000);
+
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    let month = months[time.getMonth()];
+    let year = time.getFullYear();
+    let date = time.getDate();
+    let hour = time.getHours();
+    let mins = '0' + time.getMinutes();
+    let second = '0' + time.getSeconds();
+    let formattedTime = date + ' ' + month + ' ' + year + ' ' + hour + ':' + mins.substr(-2);
+
+    console.log(` ** \n Upcoming ISS flyover on ${formattedTime} \n **`);
+  });
 });
 
 
