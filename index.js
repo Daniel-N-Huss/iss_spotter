d5da976ba541474c4416e7799f4dfb9f4da1b3bd
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-08 04:51:29 +0000

    clean up file from old test code and final lint

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSPass } = require('./iss');
 const { nextISSTimesForMyLocation } = require('./iss');
 
 
@@ -27,35 +24,3 @@ nextISSTimesForMyLocation((error, passTimes) => {
     console.log(` ** \n Upcoming ISS flyover on ${formattedTime} \n **`);
   });
 });
-
-
-//49.25000 & -123.13330
-//45.44.90.151
-
-// holdover test code
-
-// fetchMyIP((error, ip) => {
-// if (error) {
-// console.log("It didn't work!", error);
-// return;
-// }
-
-// console.log('It worked! Returned IP: ', ip);
-// });
-
-
-// fetchCoordsByIP('45.44.90.151', (error, data) => {
-//   if (error) {
-//     console.log("It didn't work! ", error);
-//     return;
-//    }
-//    console.log(`It worked! Returned coordinated: ${data.latitude} & ${data.longitude}`);
-// });
-
-//fetchISSPass({latitude: 49.25000, longitude: -123.13330}, (error, data) => {
-//  if (error) {
-//    console.log('It did not work! ', error);
-//  } else {
-//    console.log("It worked! ", data);
-//  }
-//});
